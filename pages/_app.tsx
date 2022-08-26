0f9f9a233a1ad16e638e00ae726fe835f6db24a8
Serbrylex <sbryanmadridn@gmail.com>
2022-08-26 18:04:58 -0500

    First version, integrating with success the injector, web3, web3-react, chakram, next and typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ChakraProvider } from '@chakra-ui/react'
+import { Web3ReactProvider } from '@web3-react/core'
+
+import { getLibrary } from '../config/web3'
+
+import MainLayout from '../layouts/main'
+
+function MyApp({ Component, pageProps }: AppProps) {  
+
+  return (
+    <ChakraProvider>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </Web3ReactProvider>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
